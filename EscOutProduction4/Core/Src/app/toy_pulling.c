1a34c8f5e4cbd3491a31e912379bcc220ac31ca2
Michał Szozdowski <mszozdowski@protonmail.com>
2024-11-03 19:13:36 +0100

    change sensor flag switching

--- EscOutProduction4/Core/Src/app/toy_pulling.c
+++ EscOutProduction4/Core/Src/app/toy_pulling.c
@@ -13,7 +13,7 @@
 #include "app/reset.h"
 
 static void SensDetected(distance_sens_t* distance_sens);
-static void SensReleased(distance_sens_t* distance_sens);
+//static void SensReleased(distance_sens_t* distance_sens);
 
 static toy_pulling_state_t toy_pulling_state = TOY_PULLING_IDLE;
 
@@ -23,7 +23,7 @@ static door_t door_puzzle;
 static distance_sens_t distance_sens;
 
 static volatile bool distance_sens_detected_flag = false;
-static volatile bool distance_sens_released_flag = false;
+//static volatile bool distance_sens_released_flag = false;
 
 void TOY_PULLING_Init(void)
 {
@@ -33,7 +33,7 @@ void TOY_PULLING_Init(void)
 
 	DISTANCE_SENS_Init(&distance_sens, DISTANCE_SENS_GPIO_Port, DISTANCE_SENS_Pin, 1);
 	DistanceSensDetectedCallback(SensDetected);
-	DistanceSensReleasedCallback(SensReleased);
+	//DistanceSensReleasedCallback(SensReleased);
 	if(reset_cause_get() == RESET_CAUSE_EXTERNAL_RESET_PIN_RESET)
 	{
 		DOOR_Open(&door_puzzle);
@@ -50,8 +50,7 @@ void TOY_PULLING_Process(void)
 		case TOY_PULLING_IDLE:
 			if(distance_sens_detected_flag == true)
 			{
-				distance_sens_detected_flag = false;
-				distance_sens_released_flag = false;
+				//distance_sens_released_flag = false;
 				tick = HAL_GetTick();
 				toy_pulling_state = TOY_PULLING_SENS_DETECTED;
 			}
@@ -69,6 +68,7 @@ void TOY_PULLING_Process(void)
 		case TOY_PULLING_WAIT:
 			if(HAL_GetTick() - tick > TOY_PULLING_NEXT_GAME_DELAY)
 			{
+				distance_sens_detected_flag = false;
 				toy_pulling_state = TOY_PULLING_IDLE;
 			}
 			break;
@@ -80,7 +80,7 @@ static void SensDetected(distance_sens_t* distance_sens)
 	distance_sens_detected_flag = true;
 }
 
-static void SensReleased(distance_sens_t* distance_sens)
-{
-	distance_sens_released_flag = true;
-}
+//static void SensReleased(distance_sens_t* distance_sens)
+//{
+//	distance_sens_released_flag = true;
+//}
