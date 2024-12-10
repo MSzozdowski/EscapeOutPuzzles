c42afac56f324efbf26233b5df48d8a8e59b1174
Michał Szozdowski <mszozdowski@protonmail.com>
2024-12-10 21:16:13 +0100

    change step motor to servo

--- ClockStepMotor/ClockStepMotor/ClockStepMotor.ino
+++ ClockStepMotor/ClockStepMotor/ClockStepMotor.ino
@@ -1,20 +1,30 @@
-#define REED_SW_SENSOR 7
-#define IN1 8
-#define IN2 9
-#define IN3 10
-#define IN4 11
-#define RELAY 12
-#define REED_SW 2
+#include <Servo.h>
 
-#define SOLUTION_MINUTE 30
-#define CONFIRMATION_TIME 5000
+//Pin defines
+#define REED_SW_DETECT_HAND_PIN 6
+#define REED_SW_DETECT_MOVE_PIN 7
+#define RELAY 8
+#define SERVO 9
+
+#define SERWO_STOP_VALUE 93
+#define SERWO_GO_VALUE 91
+#define SERWO_START_STOP_DELAY 20
+
 #define DEBUG_SENS_DISP_TIME 1500
-#define DEBUG_ANGLE_DISP_TIME 25
-#define DEBUG_SOL_DISP_TIME 500
+#define CONFIRMATION_TIME 5000
+
+//Functions
+void ServoProcess(void);
+
+//Typedefs
+typedef enum
+{
+  SERWO_STOP,
+  SERWO_GO
+} serwo_state_t;
 
 typedef enum {
   PUZZLE_WAIT_FOR_SENS,
-  PUZZLE_SENS_DETECTED,
   PUZZLE_DOOR_OPEN
 } puzzle_state_t;
 
@@ -24,53 +34,44 @@ typedef enum
   LAST_REED_SW_WAS_LOW
 }reed_sw_state_t;
 
-const int stepSequence[8][4] = {
-  { 1, 0, 0, 0 },
-  { 1, 1, 0, 0 },
-  { 0, 1, 0, 0 },
-  { 0, 1, 1, 0 },
-  { 0, 0, 1, 0 },
-  { 0, 0, 1, 1 },
-  { 0, 0, 0, 1 },
-  { 1, 0, 0, 1 }
-};
-
-const int stepsPerRevolution = 4076;
-int currentStep = 0;
-float currentAngle = 0;
-float targetAngle = 0;
-unsigned long tick = 0;
-unsigned long debug_tick_sens = 0;
-unsigned long debug_tick_angle = 0;
-unsigned long debug_tick_solution = 0;
-
+//Variables
+//Servo
+Servo myservo;
+serwo_state_t serwo_state = SERWO_STOP;
+//Puzzle
 puzzle_state_t puzzle_state = PUZZLE_WAIT_FOR_SENS;
 reed_sw_state_t reed_sw_state = LAST_REED_SW_WAS_HIGH;
-
+uint8_t move = 0;
 uint8_t solution_flag = 0;
 
+//Timers
+unsigned long debug_tick_sens = 0;
+unsigned long debug_tick_solution = 0;
+unsigned long tick = 0;
+
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(RELAY, OUTPUT);
-  pinMode(REED_SW, INPUT_PULLUP);
-  pinMode(REED_SW_SENSOR, INPUT_PULLUP);
+  pinMode(REED_SW_DETECT_HAND_PIN, INPUT_PULLUP);
+  pinMode(REED_SW_DETECT_MOVE_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Clock puzzle init");
+  pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, HIGH);
+  myservo.attach(SERVO);
+  Serial.begin(9600);
 }
 
 void loop() {
-  uint8_t sensor_value = digitalRead(REED_SW_SENSOR);
-  uint8_t reed_value = digitalRead(REED_SW);
+  ServoProcess();
+  uint8_t sensor_value = digitalRead(REED_SW_DETECT_MOVE_PIN);
+  uint8_t reed_value = digitalRead(REED_SW_DETECT_HAND_PIN);
 
   switch (puzzle_state) {
     case PUZZLE_WAIT_FOR_SENS:
       if (sensor_value == LOW) {
-        puzzle_state = PUZZLE_SENS_DETECTED;
+        move = 1;
+      }
+      else
+      {
+        move = 0;
       }
 
       if (millis() - debug_tick_sens >= DEBUG_SENS_DISP_TIME) {
@@ -105,50 +106,45 @@ void loop() {
       }
       break;
 
-    case PUZZLE_SENS_DETECTED:
-      moveToAngleForward(targetAngle++);
-      if (targetAngle >= 360)
-        targetAngle = 0;
-
-      puzzle_state = PUZZLE_WAIT_FOR_SENS;
-      tick = millis();
-      break;
-
     case PUZZLE_DOOR_OPEN:
       digitalWrite(RELAY, LOW);
       delay(1000);
       digitalWrite(RELAY, HIGH);
-
-      targetAngle = targetAngle - 50;
-      moveToAngleForward(targetAngle);
-
+      myservo.write(SERWO_GO_VALUE);
+      delay(4500);
       puzzle_state = PUZZLE_WAIT_FOR_SENS;
       break;
   }
 }
 
-void setStep(int stepIndex) {
-  digitalWrite(IN1, stepSequence[stepIndex][0]);
-  digitalWrite(IN2, stepSequence[stepIndex][1]);
-  digitalWrite(IN3, stepSequence[stepIndex][2]);
-  digitalWrite(IN4, stepSequence[stepIndex][3]);
-}
+void ServoProcess(void)
+{
+  static unsigned long tick_servo = millis();
+  if(move)
+  {
+    switch (serwo_state) {
+    case SERWO_STOP:
+    if(millis() - tick_servo > SERWO_START_STOP_DELAY)
+    {
+      myservo.write(SERWO_STOP_VALUE);
+      tick_servo = millis();
+      serwo_state = SERWO_GO;
+    }
 
-void moveStepsReverse(int steps) {
-  for (int i = 0; i < steps; i++) {
-    currentStep = (currentStep - 1 + 8) % 8;  // Decrement to move backward
-    setStep(currentStep);
-    delay(4);
+    break;
+
+    case SERWO_GO:
+      if(millis() - tick_servo > SERWO_START_STOP_DELAY)
+      {
+        myservo.write(SERWO_GO_VALUE);
+        tick_servo = millis();
+        serwo_state = SERWO_STOP;
+      }
+    break;
+    }
   }
-}
-
-void moveToAngleForward(float targetAngle) {
-  float targetSteps = (targetAngle / 360.0) * stepsPerRevolution;
-  float currentSteps = (currentAngle / 360.0) * stepsPerRevolution;
-
-  int stepsToMove = (int)(targetSteps - currentSteps + stepsPerRevolution) % stepsPerRevolution;
-
-  //moveStepsForward(stepsToMove);
-  moveStepsReverse(stepsToMove);
-  currentAngle = targetAngle;
+  else
+  {
+    myservo.write(SERWO_STOP_VALUE);
+  }
 }
